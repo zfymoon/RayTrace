efae780de1b14b2f65ccdf83311f97d3c54446c0
curiezhu <curiezhu@tencent.com>
2019-02-20 11:23:48 +0800

    init

---
+++ _Users_curiezhu_Documents_station_CmRayTracePh_Assets_Scripts_Main_PhoneInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Application
+{
+    public class PhoneInfo
+    {
+        public PhoneInfo()
+        {
+        }
+    }
+}
---
+++ _Users_curiezhu_Documents_station_CmRayTracePh_Assets_Scripts_RayTraceEngine_RayTraceController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RayTraceManager : MonoBehaviour
+{
+
+    struct SurfaceInfo
+    {
+        public Vector3 albedo;
+        public Vector3 specular;
+        public float smoothness;
+        public Vector3 emission;
+    }
+    struct RayNode
+    {
+        Ray ray;
+        RaycastHit hit;
+        SurfaceInfo surfaceInfo;
+        Ray[] newRayList;
+    }
+
+
+    public void Awake()
+    {
+
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
